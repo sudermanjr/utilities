927ccf1bc8e2ec3e5e56981bce4a3b80e92d5707
Andrew Suderman <andrew.suderman@readytalk.com>
2018-03-06 21:57:26 -0700

    Adding qemu binary

--- build.sh
+++ build.sh
@@ -24,6 +24,14 @@ shift $((OPTIND-1))
 
 [ "$1" = "--" ] && shift
 
+# install qemu-user-static
+if [ -n "${QEMU_ARCH}" ]; then
+    if [ ! -f x86_64_qemu-${QEMU_ARCH}-static.tar.gz ]; then
+        wget -N https://github.com/multiarch/qemu-user-static/releases/download/${QEMU_VER}/x86_64_qemu-${QEMU_ARCH}-static.tar.gz
+    fi
+    tar -xvf x86_64_qemu-${QEMU_ARCH}-static.tar.gz -C $ROOTFS/usr/bin/
+fi
+
 TMP=tmp
 ROOTFS=rootfs
 
