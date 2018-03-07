4b2b966d62040bfbcaf4dfc1ee117d5ba799e7a2
Andrew Suderman <andrew.suderman@readytalk.com>
2018-03-06 22:16:53 -0700

    Didn't need that line

--- build.sh
+++ build.sh
@@ -31,7 +31,6 @@ if [ -n "${QEMU_ARCH}" ]; then
     if [ ! -f x86_64_qemu-${QEMU_ARCH}-static.tar.gz ]; then
         wget -N https://github.com/multiarch/qemu-user-static/releases/download/${QEMU_VER}/x86_64_qemu-${QEMU_ARCH}-static.tar.gz
     fi
-    tar -xvf x86_64_qemu-${QEMU_ARCH}-static.tar.gz -C $ROOTFS/usr/bin/
 fi
 
 # add qemu-user-static binary
