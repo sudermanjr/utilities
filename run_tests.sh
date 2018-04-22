a41522ead2b207801c97e38e24bcca342c842c48
Andrew Suderman <andrew.suderman@readytalk.com>
2018-04-21 23:44:06 -0600

    Cleaning up testing stuff

--- run_tests.sh
+++ run_tests.sh
@@ -5,16 +5,18 @@ command_list=("kops version"
     "helm version --client"
     "vault version")
 
-mkdir -p test-results/basic
-echo "Utility Versions:\n\n" > test-results/basic/versions.txt
+mkdir -p test-results
+echo "Utility Versions:" > test-results/versions.txt
+echo "" >> test-results/versions.txt
+echo "" >> test-results/versions.txt
 errors=0
 for ((i = 0; i < ${#command_list[@]}; i++))
 do
-    echo "Running '${command_list[$i]}':" >> test-results/basic/versions.txt 
-    docker run --rm  $1 ${command_list[$i]} >> test-results/basic/versions.txt
+    echo "Running '${command_list[$i]}':" >> test-results/versions.txt 
+    docker run --rm  $1 ${command_list[$i]} >> test-results/versions.txt
     if [ $? -ne 0 ]
     then
-      echo "Test '${command_list[$i]}' FAILED!" >> test-results/basic/errors.txt
+      echo "Test '${command_list[$i]}' FAILED!" >> test-results/errors.txt
       ((errors+=1))
     fi
 done
