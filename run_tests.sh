013f2dc1d46c5aa6cc0ea8f92b58b2dc3e787dd4
Andrew Suderman <andrew.suderman@readytalk.com>
2018-04-21 23:33:10 -0600

    Trying out saving tests

--- run_tests.sh
+++ run_tests.sh
@@ -5,13 +5,18 @@ command_list=("kops version"
     "helm version --client"
     "vault version")
 
+mkdir -p test-results/basic
+echo "Utility Versions:\n\n" > test-results/basic/versions.txt
+errors=0
 for ((i = 0; i < ${#command_list[@]}; i++))
 do
-    echo "Running '${command_list[$i]}':"
-    docker run --rm  $1 ${command_list[$i]}
+    echo "Running '${command_list[$i]}':" >> test-results/basic/versions.txt 
+    docker run --rm  $1 ${command_list[$i]} >> test-results/basic/versions.txt
     if [ $? -ne 0 ]
     then
-      echo "Test '${command_list[$i]}' FAILED!"
-      exit 1
+      echo "Test '${command_list[$i]}' FAILED!" >> test-results/basic/errors.txt
+      ((errors+=1))
     fi
 done
+
+exit $errors
