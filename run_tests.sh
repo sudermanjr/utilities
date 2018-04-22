38277d650ad4917bfdd1d8af68555d6fc18a4a20
Andrew Suderman <andrew.suderman@readytalk.com>
2018-04-21 22:50:12 -0600

    Fixing test script to exit on failure

--- run_tests.sh
+++ run_tests.sh
@@ -2,11 +2,16 @@
 
 command_list=("kops version"
     "kubectl version --client"
-    "kops version")
+    "kops version"
+    "helm version --client")
 
 for ((i = 0; i < ${#command_list[@]}; i++))
 do
     echo "Running '${command_list[$i]}':"
     docker run --rm  $1 ${command_list[$i]}
-    echo "Exit Code: $?"
+    if [ $? -ne 0 ]
+    then
+      echo "Test '${command_list[$i]}' FAILED!"
+      exit 1
+    fi
 done
